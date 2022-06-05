881488aa3fd51dd5119d90d43ed9c7d5ccc1426a
jbird2021 <90277731+jbird2021@users.noreply.github.com>
2022-06-04 23:18:16 -0400

    Update log.py

--- log.py
+++ log.py
@@ -1,3 +1,4 @@
+#FROM GABE
 import json
 from flask import Flask, escape, request
 app = Flask(__name__)
@@ -7,6 +8,40 @@ def log():
     print(data)
     return ''
 
-
-
+# create a py file on the controller server with the above. Then run it with the command below. 
+# If flask is not installed, use pip3 to install.
 # FLASK_APP=log flask run -h 0.0.0.0 -p 8080
+
+
+#------------------------------------------------------------------------------------------------#
+# FROM BEN
+import json
+from flask import Flask, escape, request
+import logging
+app = Flask(__name__)
+logging.basicConfig(filename='tower_elk.log', level=logging.DEBUG)
+@app.route('/', methods=['POST'])
+def log():
+    data = json.loads(request.data)
+    print(data)
+    return ''
+
+# create a py file on the controller server with the above. Then run it with the command below. 
+# If flask is not installed, use pip3 to install.
+# FLASK_APP=log flask run -h 0.0.0.0 -p 8080
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
